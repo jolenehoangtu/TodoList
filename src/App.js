7101ae4782dd3acd602cd52e790990529145aae6
Tu Hoang <TuHoang@Tus-MacBook-Air.local>
2019-01-14 10:41:59 +0200

    update functionaility

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
+import Todo from "./components/Todo"
+import AddTodo from './components/AddTodo';
 import './App.css';
 
 class App extends Component {
+  state={
+    todos:[
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Breakfast with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Walk dog around',
+        completed: false
+      }
+    ]
+  }
+  //Toggle Complete - check box
+  markComplete = (id) =>{
+    this.setState({todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+
+    })});
+  }
+
+  //Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  }
+
+  //Add todo
+  addTodo =(title) =>{
+    const newTodo = {
+      id: 4,
+      title: title,
+      complete: false
+    }
+    this.setState({todos: [...this.state.todos, newTodo] });
+  }
+
   render() {
+    console.log(this.state.todos)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container"> 
+        <Header/> 
+        <AddTodo addTodo={this.addTodo}/>
+        <Todo todos={this.state.todos} 
+        markComplete={this.markComplete}
+        delTodo = {this.delTodo}/>
+        </div>
       </div>
     );
   }
